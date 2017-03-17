2d6d0693bd3a8a07cb931e33524fe23e51c7a5e4
Tiago Oliveira <tiago@econtabil.com>
2017-03-16 21:59:14 -0300

    Teste1

---
+++ src/com/opet/main.java
@@ -0,0 +1,16 @@
+package com.opet;
+
+public class main
+{
+
+    public static void main(String[] args)
+    {
+        int rep = 1;
+        while(rep < 100) {
+            System.out.println(rep);
+            rep++;
+        }
+
+    }
+
+}
